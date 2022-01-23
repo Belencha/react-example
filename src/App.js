059c017fbc296d62680f0c16b258baf6b1695644
Belencha <belencha@DESKTOP-1UO1C05.localdomain>
2022-01-23 23:04:02 +0100

    Add Dashboard template

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import StyledDiv from "./components/StyledDiv/StyledDiv";
 import CustomText from "./components/CustomText/CustomText";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -15,13 +16,13 @@ function App() {
           <br />
           Segunda tarea
         </p>
-        
       </header>
       <section className="App-section">
         <Button variant="contained">Hello world</Button>
         <StyledDiv />
         <CustomText text="and what about now?" component="p" wannabe="h1"/>
       </section>
+      <Dashboard />
     </div>
   );
 }
