d36b5c7ecf7d53d19897d46adbe485c078f039e7
Belencha <belencha@DESKTOP-1UO1C05.localdomain>
2022-01-23 20:57:47 +0100

    Add CSS reset included in MUI material

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
