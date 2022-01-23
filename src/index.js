47b2f9b91cd258a86ce1ee2ce1ba5aee803eb10c
Belencha <belencha@DESKTOP-1UO1C05.localdomain>
2022-01-23 21:02:14 +0100

    Use of Roboto as a font-family

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
 ReactDOM.render(
