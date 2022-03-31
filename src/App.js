8f88c82bdb4e8184fc19464184ce36060fe6f825
Belen Moreno <belenmore@gmail.com>
2022-03-31 22:37:34 +0200

    Relocate dashboard in homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from '@mui/material/Button';
-import logo from "./logo.svg";
 import "./App.css";
 import StyledDiv from "./components/StyledDiv/StyledDiv";
 import CustomText from "./components/CustomText/CustomText";
@@ -9,20 +8,12 @@ import Dashboard from "./components/Dashboard/Dashboard";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Primera tarea
-          <br />
-          Segunda tarea
-        </p>
-      </header>
+      <Dashboard />
       <section className="App-section">
         <Button variant="contained">Hello world</Button>
         <StyledDiv />
         <CustomText text="and what about now?" component="p" wannabe="h1"/>
       </section>
-      <Dashboard />
     </div>
   );
 }
