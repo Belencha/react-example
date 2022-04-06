cd179d7aa16919632857b25792cf8967b2e26636
Belen Moreno <belenmore@gmail.com>
2022-04-06 18:07:47 +0200

    Containarize the component for maxwidth

--- src/components/FilterableProductTable/FilterableProductTable.jsx
+++ src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import ProductTable from '../ProductTable/ProductTable';
 import SearchBar from '../SearchBar/SearchBar';
+import Container from '@mui/material/Container';
 
 const FilterableProductTable = () => {
     return (
-        <>
+        <Container maxWidth="lg">
             <SearchBar />
             <ProductTable />
-        </>
+        </Container>
     );
 }
 
